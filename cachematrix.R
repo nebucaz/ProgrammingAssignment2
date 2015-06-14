f3c4916f1ac5513489f01ece0d1befcf3fdd0880
Oliver Hofer <git@pageagent.com>
2015-06-14 17:34:48 +0200

    Added description

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,20 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two following functions can be used to calculate and cache 
+## the inverse of a matrix. The first function is a factory-function 
+## that creates a special matrix that is able to cache its inverse matrix
+## once calculated (and assigned using setinv())
+## The second function (cacheSolve) is used to
+## return the inverse of the matrix by using the
+## factory-instance
 
-## caching the inverse of a matrix 
-## function creating a matrix-caching object
+## This factory-function creates and returns a matrix-caching object
+## with 4 functions to set/get the matrix and to set/get the inverse
+## of the matrix
+##
+## functions of the returned object
+## set: set the matrix
+## get: get the matrix
+## setinv: set the inverse of the matrix
+## getinv: get the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
   inv_x <- NULL
   set <- function(y) {
@@ -18,11 +30,11 @@ makeCacheMatrix <- function(x = matrix()) {
        getinv = getinv)
 }
 
-## Return a matrix that is the inverse of 'x'
-##
-## This function computes theinverse of the special"matrix" returned by 
+## Return a matrix that is the inverse of 'x' by
+## This function computes the inverse of the "special matrix" returned by 
 ## makeCacheMatrix above. If the inverse has already been calculated 
-## (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
+## (and the matrix has not changed), then cacheSolve retrieves the inverse 
+## from the cache.
 ## It is assumed that the matrix supplied is always invertible
 
 cacheSolve <- function(x, ...) {
